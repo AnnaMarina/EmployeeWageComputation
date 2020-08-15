8e3edb40d6143b929f374525baad6d4a9fd98a22
AnnaMarina <anna.marina996@gmail.com>
2020-08-15 19:54:13 +0530

    Storing wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,14 +6,27 @@
 	wageph=20;
 #wage per hour =20
 #full day hour =8
+#maximum working hours=200
+#maximum working days = 20
+	maxhour=0;
+	maxwage=0;
+	day=0;
+
+	while [ $maxhour -le 100 ]
+	do
+
 #empcheck checks for the number of hours .
 	empcheck=$((RANDOM%9))
-
+	maxhour=$(($maxhour+$empcheck))
 	empwage=$(($empcheck*$wageph))
-	echo " Employee was is $empwage"
+	day=$(($day+1))
+
+	echo " Wage of day $day is $empwage"
+
 
 #For calculating monthly wage
 #Assuming 20 working days per month
 
-	mwage=$(($empwage*20))
-	echo " The monthly wage for the employee is $mwage"
+	maxwage=$(($maxwage+$empwage))
+	done
+	echo " The monthly wage for the employee is $maxwage"
