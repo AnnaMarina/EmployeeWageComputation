ca8663d68efc74db777ae72ee8e32e3c9d31dc1b
AnnaMarina <anna.marina996@gmail.com>
2020-08-15 09:52:06 +0530

    Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo " WELCOME TO EMPLOYEE WAGE COMPUTATION";
+
+	echo "Checking for attendance"
+	attendance=$((RANDOM%2))
+	if [ $attendance -eq 0 ]; then
+		echo "Absent"
+		exit
+		else
+		echo " Present"
+	fi
