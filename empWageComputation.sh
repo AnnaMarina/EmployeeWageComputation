9517bcd080daad7ab2100208184cd20fdfe75a01
AnnaMarina <anna.marina996@gmail.com>
2020-08-15 10:16:27 +0530

    Calculating Monthly wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo " WELCOME TO EMPLOYEE WAGE COMPUTATION";
+	echo " WELCOME TO EMPLOYEE WAGE COMPUTATION";
 
-echo " To calculate Daily Wage "
-wageph=20;
+	echo " To calculate Daily Wage "
+	wageph=20;
 #wage per hour =20
 #full day hour =8
 #empcheck checks for the number of hours .
-empcheck=$((RANDOM%9))
+	empcheck=$((RANDOM%9))
 
-empwage=$(($empcheck*$wageph))
- echo " Employee was is $empwage"
+	empwage=$(($empcheck*$wageph))
+	echo " Employee was is $empwage"
+
+#For calculating monthly wage
+#Assuming 20 working days per month
+
+	mwage=$(($empwage*20))
+	echo " The monthly wage for the employee is $mwage"
