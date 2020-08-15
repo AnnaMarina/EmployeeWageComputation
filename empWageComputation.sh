a355064a425a2dbf06030ff4718db4f73e46351a
AnnaMarina <anna.marina996@gmail.com>
2020-08-15 10:07:24 +0530

    Employee Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo " WELCOME TO EMPLOYEE WAGE COMPUTATION";
+
+echo " To calculate Daily Wage "
+wageph=20;
+#wage per hour =20
+#full day hour =8
+#empcheck checks for the number of hours .
+empcheck=$((RANDOM%9))
+
+empwage=$(($empcheck*$wageph))
+ echo " Employee was is $empwage"
